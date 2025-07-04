a9e46377d5be35e893b6e623b09f12cb493a26ea
woohee-de <woohee.de@gmail.com>
2025-07-04 17:00:17 +0900

    레이아웃 수정완료

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,12 +2,15 @@ const projects = [
   {
     id: 'pay1004',
     title: 'Pay1004',
-    thum: '/images/pay1004/pj-pay1004-1.jpg',
     year: '2025',
     period: '2025',
-    description: `페이천사는 지역 학원과 상생을 도모하는 앱 프로덕트입니다. 학원교육의 건전한 발전과 새로운 가치를 실현하는 외국어 교육 협의회와 협업했습니다. 현금과 동일한 가치를 지닌 페이를 다양한 이벤트를 통해 적립하고, 지역 학원에서 쉽게 결제할 수 있습니다.`,
     client: 'Dooit telecom',
+    tool: 'Figma',
+    role: 'UX/UI Design',
     category: 'App design',
+    thum: '/images/pay1004/pj-pay1004-1.jpg',
+    intro: `페이천사는 지역 학원과 상생을 도모하는 앱 프로덕트입니다. 학원교육의 건전한 발전과 새로운 가치를 실현하는 외국어 교육 협의회와 협업했습니다. 현금과 동일한 가치를 지닌 페이를 다양한 이벤트를 통해 적립하고, 지역 학원에서 쉽게 결제할 수 있습니다.`,
+    
     images: {
       solo: [
         { src: '/images/pay1004/pj-pay1004-1.jpg', dialogView: false },
@@ -29,41 +32,64 @@ const projects = [
   {
     id: 'markettoo-admin',
     title: 'Markettoo Admin',
-    thum: '/images/markettoo-admin/pj-markettoo-admin-1.jpg',
     year: '2024',
     period: '2024',
-    description: `우리동네 단골마트 마켓또 앱을 관리하는 관리자 사이트입니다. 주 타겟층을 오래된 마트를 운영하는 관리자로 설정하고, 쉽고 간단하게 앱을 관리할 수 있도록 직관적으로 UIUX를 설계했습니다.
-
-두잇텔레콤에서 운영하는 웹 문자발송 서비스의 기존 고객층을 마켓또로 자연스럽게 유입하기 위해, 문자발송 서비스를 업그레이드하여 마켓또 관리자 사이트에도 반영했습니다. `,
     client: 'Dooit telecom',
+    tool: 'Figma',
+    role: 'UX/UI Design',
     category: 'Web design',
+    thum: '/images/markettoo-admin/pj-markettoo-admin-1.jpg',
+    intro: `마켓또는 전통시장과 동네마트를 위한 지역 밀착형 커머스 플랫폼입니다. 이번 프로젝트는 점주를 위한 신규 관리자 웹사이트를 설계하는 것으로, 핵심 목표는 디지털 사용에 익숙하지 않은 중장년층 운영자도 쉽게 사용할 수 있는 구조를 만드는 것이었습니다. 또한, 기존에 두잇텔레콤이 별도로 운영하던 웹 문자발송 서비스를 자연스럽게 통합하여 기존 고객을 자연스럽게 유입시키는 전략이 필요했습니다.
+    
+    “사용자들이 문자도 보내고, 상품도 등록하고, 매출도 확인할 수 있는 관리 시스템이 필요해요. 너무 복잡하지 않고, 처음 보는 사람도 쉽게 쓸 수 있었으면 좋겠어요.” 위와 같은 니즈를 바탕으로 크게 3가지 설계 방향을 정의했습니다. 첫째, 디지털에 익숙하지 않은 중장년층 관리자도 빠르게 익숙해질 수 있도록 단순하고 직관적인 정보 구조(IA)를 설계했습니다. 둘째, 마켓또의 브랜드 이미지에 맞춰 균형 잡힌 시각적 톤과 일관된 UI를 구성했습니다. 셋째, 기존 문자 발송 서비스를 사용하던 고객의 이탈을 방지하기 위해 기능, 용어, 구조를 가능한 한 그대로 유지하면서도 UI 측면에서 통합했습니다.`,
+
     images: {
       solo: [
-        { src: '/images/markettoo-admin/pj-markettoo-admin-1.jpg', dialogView: false },
-        { src: '/images/markettoo-admin/pj-markettoo-admin-2.jpg', dialogView: false },
-        { src: '/images/markettoo-admin/pj-markettoo-admin-3.jpg', dialogView: false },
-        { src: '/images/markettoo-admin/pj-markettoo-admin-4.jpg', dialogView: false },
-        { src: '/images/markettoo-admin/pj-markettoo-admin-5.jpg', dialogView: false },
-        { src: '/images/markettoo-admin/pj-markettoo-admin-6.jpg', dialogView: false },
-        { src: '/images/markettoo-admin/pj-markettoo-admin-7.jpg', dialogView: false },
-        { src: '/images/markettoo-admin/pj-markettoo-admin-8.jpg', dialogView: false },
-        { src: '/images/markettoo-admin/pj-markettoo-admin-9.jpg', dialogView: false },
-        { src: '/images/markettoo-admin/pj-markettoo-admin-10.jpg', dialogView: false },
-        { src: '/images/markettoo-admin/pj-markettoo-admin-11.jpg', dialogView: false },
-        { src: '/images/markettoo-admin/pj-markettoo-admin-12.jpg', dialogView: false },
+        {
+          src: '/images/markettoo-admin/pj-markettoo-admin-2.jpg',
+          title: 'IA : 사용자 중심 정보구조 설계',
+          desc: `‘복잡한 기능이 많다’가 아니라, ‘필요한 기능만 쉽게 쓸 수 있다’는 인식을 심어주는 UI/UX 구조를 설계하고자 했습니다. 타겟층의 사용성 특성을 반영해, 정보구조를 최소화하고 가장 자주 사용하는 기능에 쉽게 접근할 수 있도록 1Depth 중심의 메뉴 구조로 설계했습니다. 또한 기능 구분을 '홈', '상품', '주문', '문자' 등 명확하게 분류하고 용어를 단순화하여 핵심 기능을 강조했습니다.`,
+        },
+        { src: '/images/markettoo-admin/pj-markettoo-admin-3.jpg' },
+        {
+          src: '/images/markettoo-admin/pj-markettoo-admin-4.jpg',
+          title: '디자인 시스템 : UX/UI 직관성 강화',
+          desc: `레이아웃은 최소 해상도 1440px을 기준으로 제작했으며, 브라우저 가로 사이즈가 확대되는 경우 좌측 내비게이션은 고정되고 콘텐츠 영역만 유연하게 확장되도록 구성했습니다. 콘텐츠 영역은 반응형 대응을 위해 12-column 그리드 시스템을 기반으로 다양한 해상도에서 동일한 경험을 제공할 수 있도록 가변 레이아웃을 구성했습니다.
+또한, 일관된 UI 제공을 위해 디자인 시스템을 구축했습니다. 텍스트 컬러와 인터페이스 간의 명도 대비를 고려한 컬러 시스템을 통해 접근성도 함께 확보했습니다.`,
+        },
+        { src: '/images/markettoo-admin/pj-markettoo-admin-5.jpg' },
+        {
+          src: '/images/markettoo-admin/pj-markettoo-admin-6.jpg',
+          title: '문자 발송 기능 통합 및 개선',
+          desc: `두잇텔레콤의 기존 웹 문자 플랫폼을 분석한 뒤, 기존 시스템의 사용 흐름과 핵심 기능 (템플릿 등록, 주소록 관리, 발송 이력 확인)을 반영하여 재구성했습니다. 기능적 연속성은 유지하되, 브랜드 일관성 확보를 위해 UI는 마켓또 관리자 디자인 시스템에 맞게 개선했습니다. 기존 고객이 혼란 없이 전환할 수 있도록 용어/프로세스 구조를 유지하면서 UI를 개선했습니다.`,
+        },
+        { src: '/images/markettoo-admin/pj-markettoo-admin-7.jpg' },
+        { src: '/images/markettoo-admin/pj-markettoo-admin-8.jpg' },
+        {
+          src: '/images/markettoo-admin/pj-markettoo-admin-9.jpg',
+          title: '상품, 주문 관리',
+          desc: `상품 관리와 매출 관리 화면은 점주들이 가장 자주 활용하는 핵심 기능으로, 실사용 흐름을 기반으로 UX를 설계했습니다. 상품 등록은 복잡한 항목을 최소화하고 필수 입력만 먼저 보이도록 하여, 비숙련 사용자도 부담 없이 접근할 수 있도록 구성했습니다.
+또한 주문 및 매출 페이지는 단순 수치 나열이 아닌, 운영자가 빠르게 현황을 파악할 수 있도록 그래프와 텍스트를 조합해 정보의 가독성과 전달력을 높이는 데 집중했습니다.`,
+        },
+        { src: '/images/markettoo-admin/pj-markettoo-admin-10.jpg' },
+        { src: '/images/markettoo-admin/pj-markettoo-admin-11.jpg' },
+        { src: '/images/markettoo-admin/pj-markettoo-admin-12.jpg' },
       ],
-      flex: [],
+      flex: [], // 필요한 경우 사용
     },
   },
   {
     id: 'markettoo',
     title: 'Markettoo',
-    thum: '/images/markettoo/pj-markettoo.jpg',
     year: '2023',
     period: '2023',
-    description: `우리동네 단골마트를 앱에서 쉽게 쇼핑하는 마켓또 앱 디자인. 마켓또는 소상공인과 소비자를 지역공동체로 연결하는 플랫폼입니다. 로또 시스템을 차용한 마또 코너를 통해 미니게임을 도입하여 쇼핑과 캐시모으기, 마또를 아우르는 앱 전부를 일종의 미니 게임으로 연출했습니다.`,
     client: 'Dooit telecom',
+    tool: 'Figma',
+    role: 'UX/UI Design',
     category: 'App design',
+    thum: '/images/markettoo/pj-markettoo.jpg',
+    intro: `우리동네 단골마트를 앱에서 쉽게 쇼핑하는 마켓또 앱 디자인. 마켓또는 소상공인과 소비자를 지역공동체로 연결하는 플랫폼입니다. 로또 시스템을 차용한 마또 코너를 통해 미니게임을 도입하여 쇼핑과 캐시모으기, 마또를 아우르는 앱 전부를 일종의 미니 게임으로 연출했습니다.`,
+    
     images: {
       solo: [
         { src: '/images/markettoo/pj-markettoo-1.jpg', dialogView: false },
@@ -85,13 +111,16 @@ const projects = [
   {
     id: 'matthiola',
     title: 'Matthiola',
-    thum: '/images/matthi/pj-matthi.jpg',
     year: '2020',
     period: '2020',
-    description: `플라워 디자인 샵 마띠올라의 CI 리뉴얼. 
-    기존 로고와 어울리면서 단독으로도 사용할 수 있는 심볼마크를 디자인했습니다.`,
     client: 'Matthiola Flower Shop',
+    tool:'Illustrator',
+    role:'Logo Design',
     category: 'Logo design',
+    thum: '/images/matthi/pj-matthi.jpg',
+    intro: `플라워 디자인 샵 마띠올라의 CI 리뉴얼. 기존 로고와 어울리면서 단독으로도 사용할 수 있는 심볼마크를 디자인했습니다.`,
+    
+    narrowSections: [0, 1, 2, 3], // solo 섹션 기준 인덱스
     images: {
       solo: [
         { src: '/images/matthi/pj-matthi.jpg', dialogView: false },
@@ -105,13 +134,16 @@ const projects = [
   {
     id: 'jl',
     title: 'JL Corporation',
-    thum: '/images/jl/pj-jl.jpg',
     year: '2019',
     period: '2019',
-    description: `문구 사무용품 도매사 제이엘 코퍼레이션의 CI 디자인.
-알파벳 J와 L을 결합하여 견고하면서도 유연한 유통의 흐름을 표현했습니다.`,
     client: 'JL Corporation',
+    tool:'Illustrator',
+    role:'Logo Design',
     category: 'Logo design',
+    thum: '/images/jl/pj-jl.jpg',
+    intro: `문구 사무용품 도매사 제이엘 코퍼레이션의 CI 디자인. 알파벳 J와 L을 결합하여 견고하면서도 유연한 유통의 흐름을 표현했습니다.`,
+    
+    narrowSections: [0, 1, 2, 3], // solo 섹션 기준 인덱스
     images: {
       solo: [
         { src: '/images/jl/pj-jl.jpg', dialogView: false },
@@ -125,12 +157,16 @@ const projects = [
   {
     id: 'innersence',
     title: 'Innersence',
-    thum: '/images/innersence/pj-innersence.jpg',
     year: '2018',
     period: '2018',
-    description: `이너센스는 제이엘 코퍼레이션의 자사 도매 브랜드로 이너웨어를 중심으로 문구, 사무 용품을 판매하고 있습니다. 자사몰로 사용하고 있는 스토어팜의 배너와 제품 상세 페이지를 작업했습니다.`,
     client: 'JL Corporation',
+    tool:'Photoshop',
+    role:'Product Detail Page design',
     category: 'Product Detail Page design',
+    thum: '/images/innersence/pj-innersence.jpg',
+    intro: `이너센스는 제이엘 코퍼레이션의 자사 도매 브랜드로 이너웨어를 중심으로 문구, 사무 용품을 판매하고 있습니다. 자사몰로 사용하고 있는 스토어팜의 배너와 제품 상세 페이지를 작업했습니다.`,
+    
+    narrowSections: [0, 1, 2], // solo 섹션 기준 인덱스
     images: {
       solo: [
         { src: '/images/innersence/pj-innersence.jpg', dialogView: false },
@@ -138,24 +174,38 @@ const projects = [
         { src: '/images/innersence/pj-innersence-2.gif', dialogView: false },
       ],
       flex: [
-        { src: '/images/innersence/pj-innersence-detail-vivian.jpg', dialogView: true },
-        { src: '/images/innersence/pj-innersence-detail-aqua.jpg', dialogView: true },
-        { src: '/images/innersence/pj-innersence-detail-elm.jpg', dialogView: true },
-        { src: '/images/innersence/pj-innersence-detail-kakao.jpg', dialogView: true },
+        {
+          src: '/images/innersence/pj-innersence-detail-vivian.jpg',
+          dialogView: true,
+        },
+        {
+          src: '/images/innersence/pj-innersence-detail-aqua.jpg',
+          dialogView: true,
+        },
+        {
+          src: '/images/innersence/pj-innersence-detail-elm.jpg',
+          dialogView: true,
+        },
+        {
+          src: '/images/innersence/pj-innersence-detail-kakao.jpg',
+          dialogView: true,
+        },
       ],
     },
   },
   {
     id: 'tym',
     title: 'Magazine tym',
-    thum: '/images/tym/pj-tym.jpg',
     year: '2016',
     period: '2016-17',
-    description: `퍼스널 트레이닝 센터 TEAMYOONGYM에서 전국 30여개 지점에 배포한 월간지 사보 tym 디자인. 숨겨진 어플, 세계의 기상천외한 사건, 다이어트 레시피, 영화소개, 생활 칼럼, 간단한 트레이닝법 페이지를 전담했습니다.
-
-    또한 TEAMYOONGYM의 자사 브랜드이자 단백질 보충제 브랜드 TYN에서 건강하고 아름다운 몸을 위한 보충제 제품 포스터를 디자인하여 사보 콘텐츠로 활용했습니다.`,
     client: 'teamyoongym',
+    tool:'InDesign',
+    role:'Editorial design',
     category: 'Editorial design',
+    thum: '/images/tym/pj-tym.jpg',
+    intro: `퍼스널 트레이닝 센터 TEAMYOONGYM에서 전국 30여개 지점에 배포한 월간지 사보 tym 디자인. 숨겨진 어플, 세계의 기상천외한 사건, 다이어트 레시피, 영화소개, 생활 칼럼, 간단한 트레이닝법 페이지를 전담했습니다. 또한 TEAMYOONGYM의 자사 브랜드이자 단백질 보충제 브랜드 TYN에서 건강하고 아름다운 몸을 위한 보충제 제품 포스터를 디자인하여 사보 콘텐츠로 활용했습니다.`,
+    
+    narrowSections: [0, 1, 2, 3, 4,5], // solo 섹션 기준 인덱스
     images: {
       solo: [
         { src: '/images/tym/pj-tym.jpg', dialogView: false },
@@ -166,7 +216,7 @@ const projects = [
         { src: '/images/tym/pj-tym-5.jpg', dialogView: false },
       ],
       flex: [],
-    }
+    },
   },
 ];
 
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -11,67 +11,87 @@ function Detail() {
   return (
     <div className="detail">
       <div className="detail-head inner-text">
-        <h2 className="detail-title">Markettoo Admin</h2>
+        <h2 className="detail-title">{project.title}</h2>
 
         <div className="detail-infos">
           <dl className="detail-infos__dl">
             <dt className="detail-infos__dt">co.</dt>
-            <dd className="detail-infos__dd">Dooit telecom</dd>
+            <dd className="detail-infos__dd">{project.client}</dd>
           </dl>
 
           <dl className="detail-infos__dl">
             <dt className="detail-infos__dt">year</dt>
-            <dd className="detail-infos__dd">2024</dd>
+            <dd className="detail-infos__dd">{project.year}</dd>
           </dl>
 
           <dl className="detail-infos__dl">
             <dt className="detail-infos__dt">tool</dt>
-            <dd className="detail-infos__dd">Figma</dd>
+            <dd className="detail-infos__dd">{project.tool}</dd>
           </dl>
 
           <dl className="detail-infos__dl">
             <dt className="detail-infos__dt">role</dt>
-            <dd className="detail-infos__dd">UX/UI Design (100%)</dd>
+            <dd className="detail-infos__dd">{project.role}</dd>
           </dl>
         </div>
       </div>
 
       <div className="detail-main">
-        <img
-          className="detail-main__img"
-          src="/images/markettoo-admin/pj-markettoo-admin-1.jpg"
-          alt=""
-        />
+        <img className="detail-main__img" src={project.thum} alt="" />
 
-        <p className="detail__intro inner-text">
-          마켓또는 전통시장과 동네마트를 위한 지역 밀착형 커머스 플랫폼입니다.
-          이번 프로젝트는 점주를 위한 신규 관리자 웹사이트를 설계하는 것으로,
-          핵심 목표는 디지털 사용에 익숙하지 않은 중장년층 운영자도 쉽게 사용할
-          수 있는 구조를 만드는 것이었습니다. 또한, 기존에 두잇텔레콤이 별도로
-          운영하던 웹 문자발송 서비스를 자연스럽게 통합하여 기존 고객을
-          자연스럽게 유입시키는 전략이 필요했습니다.
-          <br />
-          <br />
-          “사용자들이 문자도 보내고, 상품도 등록하고, 매출도 확인할 수 있는 관리
-          시스템이 필요해요. 너무 복잡하지 않고, 처음 보는 사람도 쉽게 쓸 수
-          있었으면 좋겠어요.” 해당 니즈를 바탕으로 크게 3가지 설계 방향을
-          정의했습니다. 첫째, 디지털에 익숙하지 않은 중장년층 관리자도 빠르게
-          익숙해질 수 있도록 단순하고 직관적인 정보 구조(IA)를 설계했습니다.
-          둘째, 마켓또의 브랜드 이미지에 맞춰 균형 잡힌 시각적 톤과 일관된 UI를
-          구성했습니다. 셋째, 기존 문자 발송 서비스를 사용하던 고객의 이탈을
-          방지하기 위해 기능, 용어, 구조를 가능한 한 그대로 유지하면서도 UI
-          측면에서 통합했습니다.
-          <br />
-          디자이너 1명, 개발자 1명으로 구성된 소규모 팀 프로젝트로, 초기 IA
-          설계부터 UI 컴포넌트 제작, 기능 우선순위 정의 과정을 주도했습니다.
-          피그마를 통해 UI 기획서 + 컴포넌트 가이드 + 디자인 시안을 함께
-          문서화하여 개발 협업 효율을 극대화했습니다.
-        </p>
+        <p className="detail__intro inner-text">{project.intro}</p>
       </div>
 
       <div className="detail-content">
         <div className="inner">
-          <div className="detail-sec">
+          {project.images?.solo?.map(({ src, title, desc }, idx) => {
+            const isNarrow = project.narrowSections?.includes(idx);
+            return (
+              <div
+                className={`detail-sec ${isNarrow ? 'detail-sec--narrow' : ''}`}
+                key={`solo-${idx}`}
+              >
+                <img
+                  className="detail__img"
+                  src={src}
+                  alt={`${project.title} 이미지 ${idx + 1}`}
+                />
+                {title && desc && (
+                  <div className="detail-sec__desc inner-text">
+                    <h3 className="detail-sec__desc-title">{title}</h3>
+                    <p className="detail-sec__desc-p">{desc}</p>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+
+          {project.images?.flex &&
+            [...Array(Math.ceil(project.images.flex.length / 2))].map(
+              (_, groupIdx) => {
+                const startIdx = groupIdx * 2;
+                const group = project.images.flex.slice(startIdx, startIdx + 2);
+                return (
+                  <div
+                    className="detail-sec detail-sec--flex"
+                    key={`flex-${groupIdx}`}
+                  >
+                    {group.map(({ src, dialogView }, idx) => (
+                      <img
+                        key={`flex-${startIdx + idx}`}
+                        className="detail__img"
+                        data-dialog={dialogView ? true : undefined}
+                        data-hover="true"
+                        src={src}
+                        alt={`${project.title} 이미지 ${startIdx + idx + 1}`}
+                      />
+                    ))}
+                  </div>
+                );
+              },
+            )}
+
+          {/* <div className="detail-sec">
             <img
               className="detail__img"
               src="/images/markettoo-admin/pj-markettoo-admin-2.jpg"
@@ -90,130 +110,7 @@ function Detail() {
                 명확하게 분류하고 용어를 단순화하여 핵심 기능을 강조했습니다.
               </p>
             </div>
-          </div>
-
-          <div className="detail-sec">
-            <img
-              className="detail__img"
-              src="/images/markettoo-admin/pj-markettoo-admin-3.jpg"
-              alt=""
-            />
-          </div>
-
-          <div className="detail-sec">
-            <img
-              className="detail__img"
-              src="/images/markettoo-admin/pj-markettoo-admin-4.jpg"
-              alt=""
-            />
-
-            <div className="detail-sec__desc inner-text">
-              <h3 className="detail-sec__desc-title">
-                디자인 시스템 : UX/UI 직관성 강화
-              </h3>
-              <p className="detail-sec__desc-p">
-                레이아웃은 최소 해상도 1440px을 기준으로 제작했으며, 브라우저
-                가로 사이즈가 확대되는 경우 좌측 내비게이션은 고정되고 콘텐츠
-                영역만 유연하게 확장되도록 구성했습니다. 콘텐츠 영역은 반응형
-                대응을 위해 12-column 그리드 시스템을 기반으로 다양한 해상도에서
-                동일한 경험을 제공할 수 있도록 가변 레이아웃을 구성했습니다.
-                또한, 일관된 UI 제공을 위해 디자인 시스템을 구축했습니다. 텍스트
-                컬러와 인터페이스 간의 명도 대비를 고려한 컬러 시스템을 통해
-                접근성도 함께 확보했습니다.
-              </p>
-            </div>
-          </div>
-
-          <div className="detail-sec">
-            <img
-              className="detail__img"
-              src="/images/markettoo-admin/pj-markettoo-admin-5.jpg"
-              alt=""
-            />
-          </div>
-
-          <div className="detail-sec">
-            <img
-              className="detail__img"
-              src="/images/markettoo-admin/pj-markettoo-admin-6.jpg"
-              alt=""
-            />
-
-            <div className="detail-sec__desc inner-text">
-              <h3 className="detail-sec__desc-title">
-                문자 발송 기능 통합 및 개선
-              </h3>
-              <p className="detail-sec__desc-p">
-                두잇텔레콤의 기존 웹 문자 플랫폼을 분석한 뒤, 기존 시스템의 사용
-                흐름과 핵심 기능 (템플릿 등록, 주소록 관리, 발송 이력 확인)을
-                반영하여 재구성했습니다. 기능적 연속성은 유지하되, 브랜드 일관성
-                확보를 위해 UI는 마켓또 관리자 디자인 시스템에 맞게
-                개선했습니다. 기존 고객이 혼란 없이 전환할 수 있도록
-                용어/프로세스 구조를 유지하면서 UI를 개선했습니다.
-              </p>
-            </div>
-          </div>
-
-          <div className="detail-sec">
-            <img
-              className="detail__img"
-              src="/images/markettoo-admin/pj-markettoo-admin-7.jpg"
-              alt=""
-            />
-          </div>
-
-          <div className="detail-sec">
-            <img
-              className="detail__img"
-              src="/images/markettoo-admin/pj-markettoo-admin-8.jpg"
-              alt=""
-            />
-          </div>
-
-          <div className="detail-sec">
-            <img
-              className="detail__img"
-              src="/images/markettoo-admin/pj-markettoo-admin-9.jpg"
-              alt=""
-            />
-
-            <div className="detail-sec__desc inner-text">
-              <h3 className="detail-sec__desc-title">상품, 주문 관리</h3>
-              <p className="detail-sec__desc-p">
-                상품 관리와 매출 관리 화면은 점주들이 가장 자주 활용하는 핵심
-                기능으로, 실사용 흐름을 기반으로 UX를 설계했습니다. 상품 등록은
-                복잡한 항목을 최소화하고 필수 입력만 먼저 보이도록 하여, 비숙련
-                사용자도 부담 없이 접근할 수 있도록 구성했습니다. 또한 주문 및
-                매출 페이지는 단순 수치 나열이 아닌, 운영자가 빠르게 현황을
-                파악할 수 있도록 그래프와 텍스트를 조합해 정보의 가독성과
-                전달력을 높이는 데 집중했습니다.
-              </p>
-            </div>
-          </div>
-
-          <div className="detail-sec">
-            <img
-              className="detail__img"
-              src="/images/markettoo-admin/pj-markettoo-admin-10.jpg"
-              alt=""
-            />
-          </div>
-
-          <div className="detail-sec">
-            <img
-              className="detail__img"
-              src="/images/markettoo-admin/pj-markettoo-admin-11.jpg"
-              alt=""
-            />
-          </div>
-
-          <div className="detail-sec">
-            <img
-              className="detail__img"
-              src="/images/markettoo-admin/pj-markettoo-admin-12.jpg"
-              alt=""
-            />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
